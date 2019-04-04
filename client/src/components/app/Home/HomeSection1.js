08b763fda564653a97abb23599df8f1c858d9572
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2019-04-04 14:09:08 +0200

    maquetaision

--- client/src/components/app/Home/HomeSection1.js
+++ client/src/components/app/Home/HomeSection1.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { MDBMask, MDBView, MDBCol, MDBRow } from 'mdbreact';
 import logo from '../../../images/LOGO-white.png'
-import background from '../../../images/background-home-02.jpg'
+import background from '../../../images/background-home-05-edit.jpg'
 import './HomeSection.css'
 
 class FullPageIntroWithFixedNavbar extends React.Component {
@@ -22,13 +22,13 @@ class FullPageIntroWithFixedNavbar extends React.Component {
         <section>
           <MDBView src={background}>
             <MDBMask overlay="black-light" className="flex-center flex-column text-white">
-            <MDBRow>
-            <MDBCol md="3"></MDBCol>
-              <MDBCol md="6">
-                <h1 className="text-center"><img src={logo} className="logo" alt="logo" style={{width:2953/5, height:1181/5}} /></h1>
-                <h2 className="text-center">Nutrition - Health - Fitness</h2>
-                <br />
-                <p className="font-weight-normal">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <MDBRow className="top-class">
+            <MDBCol md="1"></MDBCol>
+              <MDBCol md="3">
+                <h1 className="text-center"><img src={logo} className="logo" alt="logo" style={{width:2953/9, height:1181/9}} /></h1>
+                <h4 className="text-center mb-5">Nutrition - Health - Fitness</h4>
+                
+                <p className="font-weight-normal px-2">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                 <Link to={"/signup"} className="btn btn-outline-light btn-account btn-rounded m-5 btn-filled"><i className="fas fa-home mr-2"></i> Signup</Link>
               </MDBCol>
               <MDBCol md="3"></MDBCol>
--- client/src/components/app/Home/HomeSection2.js
+++ client/src/components/app/Home/HomeSection2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { MDBMask, MDBView, MDBCol, MDBRow } from 'mdbreact';
 import logo from '../../../images/LOGO-white.png'
-import background from '../../../images/background-recipe-01.jpg'
+import background from '../../../images/background-recipe-01-flipped.jpg'
 import './HomeSection.css'
 
 class FullPageIntroWithFixedNavbar extends React.Component {
@@ -23,11 +23,11 @@ class FullPageIntroWithFixedNavbar extends React.Component {
           <MDBView src={background}>
             <MDBMask overlay="black-light" className="flex-center flex-column text-white">
             <MDBRow>
-            <MDBCol md="3"></MDBCol>
-              <MDBCol md="6">
-                <h2 className="text-center title-section">RECIPES</h2>
+            <MDBCol md="8"></MDBCol>
+              <MDBCol md="3">
+                <h2 className="text-center title-section mb-4">RECIPES</h2>
                 <br />
-                <p  className="font-weight-normal">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <p  className="font-weight-normal p-2 pb-4">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <Link to={"/recipes"} className="btn btn-outline-light btn-account btn-rounded m-5  btn-filled"><i className="fas fa-home mr-2"></i> Recipes</Link>
               </MDBCol>
               <MDBCol md="3"></MDBCol>
